c949e36d35118bbe398bad2c6d919493245eff4b
Julien Guillen <723506@student.dusd.net>
2024-04-15 15:22:50 -0700

    3.2 done
    4/15/24

--- Unit3Sounds&EffectsJLNP8/Assets/Scripts/SpawnManager.cs
+++ Unit3Sounds&EffectsJLNP8/Assets/Scripts/SpawnManager.cs
@@ -9,19 +9,20 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
     void SpawnObstacle()
     {
-       Instantiate(Obstacles, spawnPos, Obstacles.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(Obstacles, spawnPos, Obstacles.transform.rotation);
+        }
+
     }
 }
